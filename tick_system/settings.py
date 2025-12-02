a8efa5ef5b3a9a96eb11f2f7c14826e78662e0a6
Parth Vasdewani <imparthvasdewani@gmail.com>
2025-12-02 18:23:57 +0530

    Added celery results to the settings configuration of project to store ticks into db

--- tick_system/settings.py
+++ tick_system/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Dependencies
+    'django_celery_results',
+
     # Adding apps
     'tick_consumer',
     'tick_producer',
