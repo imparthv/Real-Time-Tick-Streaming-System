dd626eaaac9c1599cb776fde655ddfc91238be21
Parth Vasdewani <imparthvasdewani@gmail.com>
2025-12-11 08:41:21 +0530

    Made command file modular and removed redudant code.

--- tick_producer/management/commands/run_tick_producer.py
+++ tick_producer/management/commands/run_tick_producer.py
@@ -5,6 +5,7 @@ import time
 import websocket
 
 from datetime import datetime, timezone
+import time
 from django.core.management.base import BaseCommand
 from celery import Celery
 from tick_consumer.tasks import get_broker, consume_tick
@@ -29,90 +30,101 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS(f"Starting producer for broker {broker_id}"))
 
         # Fetch broker + scripts via Celery task
-        broker_data = get_broker.delay(broker_id).get()
+        broker_data = fetch_broker_data(broker_id)
 
         if "error" in broker_data:
-            self.stdout.write(self.style.ERROR(broker_data["error"]))
-            return
+            logger.error(f'Received error {broker_data['error']}')
+           
         
-        scripts = broker_data["scripts"]
+        # Fetching scripts
+        scripts = fetch_broker_scripts(broker_data)
 
-        if not scripts:
-            self.stdout.write(self.style.WARNING("No scripts found on this broker!!!!"))
-            return 
-        
-
-        # Build Binance subscription URL
-        symbols = [s["trading_symbol"].lower() for s in scripts]
-        stream_query = "/".join([f"{sym}@trade" for sym in symbols])
-        ws_url = BINANCE_WS_URL + stream_query
-
-
+        # Building url
+        ws_url = build_WS_URL(scripts)
         self.stdout.write(self.style.SUCCESS(f"Connecting to Binance URL:{ws_url}"))
 
+        # Initiating websocket connection
+        initiate_websocket_connection(ws_url, scripts)
+        handle_binance_message
 
-        # Performing auto connect
-        while True:
-            try:
-                ws = websocket.WebSocketApp(
-                    ws_url,
-                    on_message=self.script_wrapper(scripts),
-                    on_error=self.on_error,
-                    on_close=self.on_close
-                )
 
-                ws.run_forever()
+# Fetching broker scripts
+def fetch_broker_data(broker_id):
+    broker_data = get_broker.delay(broker_id).get()
+    return broker_data
 
-            except Exception as e:
-                self.stdout.write(self.style.ERROR(f"Websocket crashed:{e}"))
-                time.sleep(5)
-                self.stdout.write(self.style.WARNING("Reconnecting......"))
+def fetch_broker_scripts(broker_data):
+    scripts = broker_data["scripts"]
 
-    # Message wrapper
-    def script_wrapper(self, scripts):
-        def _handler(ws, msg):
-            self.on_message(ws, msg, scripts)
-        return _handler
+    if not scripts:
+        logger.error("No scripts found on this broker!!!!")
+    return scripts
 
-    def on_message(self, ws, message, scripts):
+# Building Binance url
+def build_WS_URL(scripts):
+    symbols = [s["trading_symbol"].lower() for s in scripts]
+    stream_query = "@trade/".join(symbols)
+    ws_url = BINANCE_WS_URL + stream_query
+    return ws_url
+
+# Initiating binance websocket connection with auto connect
+def initiate_websocket_connection(ws_url, scripts):
+
+    # Find script_id from our mapping
+    script_map = {s["trading_symbol"]: s["id"] for s in scripts}
+    while True:
         try:
-            data = json.loads(message)
+            ws = websocket.WebSocketApp(
+                ws_url,
+                on_message=lambda ws, msg : handle_binance_message(ws, msg ,script_map),
+                on_error=on_error, 
+                on_close = on_close
+                
+            )
 
-            # Binance trade stream payload
-            trade = data.get("data", {})
-
-            symbol = trade.get("s", "").lower()
-            price = trade.get("p")
-            volume = trade.get("q")
-            ts = trade.get("T")
-
-            if not symbol or not price:
-                return
-
-            # Find script_id from our mapping
-            script_map = {s["trading_symbol"]: s["id"] for s in scripts}
-            if symbol not in script_map:
-                return
-            
-            tick = {
-                "script_id": script_map[symbol],
-                "value": float(price),
-                "volume": float(volume) if volume else None,
-                "received_at": datetime.fromtimestamp(ts/1000, tz=timezone.utc).isoformat()
-            }
-
-            # Sends ticks to Celery
-            consume_tick.delay([tick])
-
+            ws.run_forever()
 
         except Exception as e:
-            # Log the error instead of raising
-            logger.error(f"Error parsing message: {e}")
+            logger.error(f"Websocket crashed:{e}")
+            time.sleep(5)
+            logger.log("Reconnecting......")
 
-    def on_error(self, ws, error):
-        print(f"WebSocket Error: {error}")
+def handle_binance_message(ws, message, script_map):
+    try:
+        # JSON formatted string.Requires dict conversion
+        data = json.loads(message).get("data", {})
 
-    def on_close(self, ws, *args):
-        print("WebSocket closed. Reconnecting...")
-            
+        symbol = data.get("s", "").lower()
+        price = data.get("p")
+        volume = data.get("q")
+        ts = data.get("T")
+
+        if not symbol or not price:
+            return
+
         
+        if symbol not in script_map:
+            return
+        
+        tick = {
+            "script_id": script_map[symbol],
+            "value": float(price),
+            "volume": float(volume) if volume else None,
+            "received_at": datetime.fromtimestamp(ts/1000, tz=timezone.utc).isoformat()
+        }
+
+
+        # Sends ticks to Celery
+        consume_tick.delay(tick)
+
+
+    except Exception as e:
+        # Log the error instead of raising
+        logger.error(f"Error parsing message: {e}")
+
+
+def on_error(ws, error):
+    print(f"WebSocket Error: {error}")
+
+def on_close(ws, *args):
+    print("WebSocket closed. Reconnecting...")
